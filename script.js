d157ed256a52e3ef13eb2e2f8bb15bb293277d78
tluong920 <47793112+tluong920@users.noreply.github.com>
2019-03-11 09:13:49 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+function status(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return Promise.resolve(response)
+  } else {
+    return Promise.reject(new Error(response.statusText))
+  }
+}
+
+function json(response) {
+  return response.json()
+}
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(status)
+  .then(json)
+  .then(function(data) {
+    console.log('Request succeeded', data);
+    let arrUsername = data.map(a => a.username);
+    arrUsername.sort((a, b) => a.length - b.length);
+    var username = ""
+    for(i =0; i < arrUsername.length; i++) {
+      username += arrUsername[i] + "<br>";
+  }
+  document.getElementById("fetchdata").innerHTML = username; 
+  }).catch(function(error) {
+    console.log('Request failed', error);
+  });
+  
+const req = new XMLHttpRequest(); 
+req.open('GET','https://jsonplaceholder.typicode.com/users'); 
+req.onload = function () { 
+    const data = JSON.parse(req.response);
+    let arrEmail = data.map(a => a.email);
+    arrEmail.sort();
+    var txt = "";
+    if (req.status == 200) { 
+        console.log (req.responseText);
+        for(i =0; i < arrEmail.length; i++) {
+            txt += arrEmail[i] + "<br>";
+        }
+        document.getElementById("output").innerHTML = txt; 
+    } else { 
+        console.log('ERROR', req.statusText); 
+    } 
+};
+req.onerror = function () { 
+    console.log('Network Error'); 
+}; 
+req.send(); 
